3ee28ab6cd8267297eba5a2b53e27a772b810b23
sanguogege <981952053@qq.com>
2018-08-10 11:10:34 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,75 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    // 日历控件的数据
+      dateInfo:'',
+      dateType:'',
+      deteAll:'',
+      hour:'',
+      rl:'',
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+    showDatePcik: function () {//显示日历控件
+        this.setData({
+            show: true,
+        })
+    },
+    submit: function (e) {//控件确定是提交
+        console.log(e.detail.deteAll)//控件时间数据大全
+        this.setData({
+            dateInfo: e.detail.dateStr,
+            dateType: e.detail.rl,
+            deteAll: JSON.stringify(e.detail.deteAll),
+            hour: e.detail.hour,
+            rl: e.detail.rl,
+        })
+    }
+})
